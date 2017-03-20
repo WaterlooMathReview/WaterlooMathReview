ab0b12f8231c4dfdf4930e7ba924ddb900d518dc
Sally <sally.dong@uwaterloo.ca>
2017-03-20 14:14:29 -0400

    updated file directories for past issues

--- archive/volii/1/code/circular-dam/import_geom.m
+++
@@ -1,8 +0,0 @@
-%this file provides the matrices p,e,t
-
-[p,e,t]=initmesh('circgeom');
-
-dlmwrite('p',p);
-dlmwrite('e',e);
-dlmwrite('t',t);
-
--- archive/volii/1/code/circular-dam/jy.m
+++
@@ -1,5 +0,0 @@
-function jy_flux = jy(h, u, v)
-
-g=9.81;
-jy_flux=[h*v,h*u*v,(1/2)*g*h*h + h*v*v];
-end 
